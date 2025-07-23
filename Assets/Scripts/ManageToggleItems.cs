7845341cfcfdf39f445ab608a4b11f0f03cda1a2
RobertD-stack <rgdewitty@aggies.ncat.edu>
2025-07-23 17:08:48 -0400

    Major Changes 7/23
    Changes
    Remove placeholder scenes and added several new scenes
    
    Bug Fixes
    Sometimes highlight material is not removed upon snapping
    Quiz data not writing to JSON file
    Text for toggle menu isn’t being masked

--- Assets/Scripts/ManageToggleItems.cs
+++ Assets/Scripts/ManageToggleItems.cs
@@ -41,7 +41,7 @@ public class ManageToggleItems : MonoBehaviour
             toggleObj.name = obj.name; // We have to do this for when search for the object
             // set the object's parent to list parent so that it gets added to the list
             toggleObj.transform.SetParent(listParent.transform, false);
-            toggleObj.GetComponentInChildren<TextMeshPro>().text = obj.name;
+            toggleObj.GetComponentInChildren<TextMeshProUGUI>().text = obj.name;
             Image img = toggleObj.transform.Find("Icon").GetComponentInChildren<Image>();
 
 
